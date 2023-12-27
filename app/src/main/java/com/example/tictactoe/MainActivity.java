52c003063cbf8dc017c2d0b5488387a472edb5bb
ErelWeinstein <erelweinstein123@gmail.com>
2023-12-27 15:51:27 +0200

    onNewGame function

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -6,12 +6,30 @@ import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    String turn;
+    String [] [] board;
+    int count;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    private void onNewGame ()
+
+    {
+        board = new String[3][3];
+        for (int row = 0; row < 3; row++)
+        {
+            for (int col=0; col<3; col++)
+            {
+                board [row] [col] = new String();
+            }
+        }
+        turn = "x";
+        count=0;
+    }
+
     public void onBtnClicked(View view)
     {
     }
