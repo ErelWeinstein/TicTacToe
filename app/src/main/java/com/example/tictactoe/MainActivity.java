d85309dc1fd2094085782d52a5d57bbe188a84a0
ErelWeinstein <erelweinstein123@gmail.com>
2023-12-29 14:09:22 +0200

    final app

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AlertDialogLayout;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -19,6 +18,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onNewGame();
     }
 
     private void onNewGame() {
@@ -43,6 +43,8 @@ public class MainActivity extends AppCompatActivity
             handleClick(0, 2, R.id.btn_02);
         } else if (id == R.id.btn_10) {
             handleClick(1, 0, R.id.btn_10);
+        } else if (id == R.id.btn_11) {
+            handleClick(1, 1, R.id.btn_11);
         } else if (id == R.id.btn_12) {
             handleClick(1, 2, R.id.btn_12);
         } else if (id == R.id.btn_20) {
@@ -65,11 +67,11 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void endGame(String s) {
+    private void endGame(String s)
+    {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("More Info");
-        String msg = "This is the message body";
-        builder.setMessage(msg);
+        builder.setMessage(s);
         builder.setPositiveButton("EXIT", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
@@ -89,49 +91,52 @@ public class MainActivity extends AppCompatActivity
     {
         for (int row = 0; row < 3; row++)
         {
-            if (board[row][0].equals(board[row][1])  && board[row][0].equals(board[row][2]))
-            {
-                return true;
-            }
-
-            for (int col = 0; col < 3; col++)
-            {
-                if (board[0][col].equals(board[1][col])  && board[0][col].equals(board[2][col]))
+            if (board[row][0].toString() ==(board[row][1]).toString())
+                if (board[row][0].toString() == (board[row][2]).toString())
                 {
                     return true;
                 }
+
+            for (int col = 0; col < 3; col++)
+            {
+                if (board[0][col].toString() ==(board[1][col]).toString())
+                    if (board[0][col].toString() == (board[2][col]).toString())
+                    {
+                        return true;
+                    }
             }
 
             for (row = 0; row < 3; row++)
             {
                 for (int col = 0; col < 3; col++)
                 {
-                    if (board[0][0].equals(board[1][1]) && board[0][0].equals(board[2][2]))
-                    {
-                        return true;
-                    }
-                    if (board[0][2].equals(board[1][1]) && board[0][2].equals(board[2][0]))
-                    {
-                        return true;
-                    }
+                    if (board[0][0].toString() ==(board[1][1]).toString())
+                        if (board[0][0].toString() == (board[2][2]).toString())
+                        {
+                            return true;
+                        }
+                    if (board[0][2].toString() ==(board[1][1]).toString())
+                        if (board[0][2].toString() == (board[2][0]).toString())
+                        {
+                            return true;
+                        }
                 }
             }
         }
         return false;
     }
 
-    private void onTurnEnd()
-    {
-        if (isWinner())
+
+
+    private void onTurnEnd() {
+        if (isWinner()) {
             endGame(turn + " won!");
-        else
-        {
+        } else {
             count++;
-            if (count == 9)
+            if (count == 9) {
                 endGame("Tie");
-            else
-            {
-                turn = (turn.equals("X") ? "O" : "X");
+            } else {
+                turn = (turn.equals("x") ? "o" : "x"); // switch turn
             }
         }
     }
